a0ebd262bcf67a8b07e2c92bf5bc5659dd31b4d8
rasmusrn <rasmusrnielsen@gmail.com>
2010-09-08 16:07:24 +0200

    Removed argumentsToArray method. Using Array.prototype.slice.call instead.

--- src/observable.js
+++ src/observable.js
@@ -9,7 +9,7 @@ var Observable = {
 			var event = arguments[0];
 			var observers = this.observers[event];
 			if(observers) {
-				var passing_arguments = this.argumentsToArray(arguments, 1);
+				var passing_arguments = Array.prototype.slice.call(arguments, 1)
 				jQuery.each(observers, function(i, observer) {
 					observer.apply(null, passing_arguments);
 				});
@@ -19,19 +19,12 @@ var Observable = {
 	bubble: function() {
 		var self = this;
 		var observable = arguments[0];
-		jQuery.each(this.argumentsToArray(arguments, 1), function(index, event) {
+		jQuery.each(Array.prototype.slice.call(arguments, 1), function(index, event) {
 			observable.subscribe(event, function() {
-				var args_array = self.argumentsToArray(arguments);
+				var args_array = Array.prototype.slice.call(arguments);
 				args_array.unshift(event);
 				self.notify.apply(self, args_array);
 			});
 		});
-	},
-	argumentsToArray: function(args, start_from) {
-		var array = [];
-		for(var i=(start_from || 0); args.length>i; i++) {
-			array.push(args[i]);
-		}
-		return array;
 	}
 };
